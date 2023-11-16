f7b2dd0de807f7f65c93f1c924e88d5e44afc5e8
Rickey Morris <rmorris.rickey@gmail.com>
2023-11-16 16:23:09 -0500

    Added form controls, which open in a modal.

--- script.js
+++ script.js
@@ -3,10 +3,14 @@
 const myLibrary = [{
     title: "My Book",
     author: "Rick",
+    pages: 200,
+    read: true,
 },
 {
     title: "Game of Thrones",
     author: "Martin",
+    pages: 400,
+    read: false,
 }];
 
 // Create Book Constructor
@@ -24,10 +28,41 @@ function addBookToLibrary(){
     // Takes information from form to build a new Book, then adds that to myLibrary array.
 }
 
+// Lists books within a table by appending each entry as a new 'tr' element with 'td' elements that contain the book's information.
+
 function displayLibrary(library){
+    // Iterate over the books of the array.
     for (let book of library) {
-        console.log(Object.keys(book));
-    }
+        // Begin row for new entry.
+        const newRow = document.createElement("tr");
+        // Traverse over each of the properties of a book and retreive their values.
+        for (let property in book) {
+            // Create cell that will contain a properties' value.
+            const propValue = document.createElement("td");
+            propValue.innerText = book[property];
+            // Add cell to the row as a child of the 'tr' element.
+            newRow.appendChild(propValue);
+        }
+        // Finally, add the entire row that contains all of the book's information to the table.
+        books.appendChild(newRow);
+    }   
 }
 
+// Select the table element.
+const books = document.getElementById("bookList");
+
+// Add way to display modal window.
+const dialog = document.getElementById("dialogBox");
+
+// Add event listner to Add Book button.
+const addBookButton = document.getElementById("addBook");
+addBookButton.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+// Button to close dialog.
+const closeDialogButton = document.getElementById("closeDialog");
+closeDialogButton.addEventListener("click", () => {
+    dialog.close();
+})
 displayLibrary(myLibrary);
