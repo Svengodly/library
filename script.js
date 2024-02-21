b8f63ce41d9d14384c985cf6072a35040d7f98c4
Rickey Morris <rmorris.rickey@gmail.com>
2024-02-20 19:13:08 -0500

    Defined addBookToLibrary function. Information entered by user gets added to the table when clicking the submit button. Checks still need to be made to validate user input prior to submission.

--- script.js
+++ script.js
@@ -28,12 +28,6 @@ Book.prototype.sayTitle = function() {
 myLibrary.push(dummyBook);
 myLibrary.push(dummyBook2);
 
-// Create function that allows user to add books to library
-
-function addBookToLibrary(){
-    // Takes information from form to build a new Book, then adds that to myLibrary array.
-}
-
 // Lists books within a table by appending each entry as a new 'tr' element with 'td' elements that contain the book's information.
 
 function displayLibrary(library){
@@ -72,17 +66,43 @@ addBookButton.addEventListener("click", () => {
 // The submit button needs to retreive all of the values of the input fields of the form and use them to create a new Book object.
 // The values of the input elements can be retreived by selecting the individual input elements by id through the HTMLInputElementAPI (document.getElementById('title') for example)
 
-// Erase the input fields upon submission of form.
 const diagForm = document.querySelector("form");
 
 diagForm.addEventListener("submit", (e) => {
+    addBookToLibrary(diagForm.querySelectorAll("input"));
+    // Erase the input fields upon submission of form.
     diagForm.reset();
 })
+
+// Create function that allows user to add books to library
+
+function addBookToLibrary(formData){
+    // Takes information from form to build a new Book, then adds that to myLibrary array.
+    // Delaring a variable to store an array of values for each input. Converting from a NodeList to an Array to gain access to map method.
+    let inputValues = Array.from(formData).map((input) => input.value);
+    const newBook = new Book(...inputValues);
+    myLibrary.push(newBook);
+    // Add new book to table. Start by creating a new row with a 'tr' element.
+    const newRow = document.createElement("tr");
+    // Traverse over each of the properties of a book and retreive their values.
+    for (let property in newBook) {
+        if (property == "sayTitle") {
+            continue;
+        }
+        // Create cell that will contain a properties' value.
+        const propValue = document.createElement("td");
+        propValue.innerText = newBook[property];
+        // Add cell to the row as a child of the 'tr' element.
+        newRow.appendChild(propValue);
+    }
+    // Finally, add the entire row that contains all of the book's information to the table.
+    books.appendChild(newRow);
+    console.log(newBook);
+}
 
 // Add a way to close dialog box
 const closeButton = document.getElementById("closeButton");
 closeButton.addEventListener("click", (e) => {
-    e.preventDefault();
     dialog.close();
 })
 
