3037ba406d4f0f6cc0ba04c272f87bb432be7665
Rickey Morris <rmorris.rickey@gmail.com>
2024-02-22 16:07:20 -0500

    Created a function that creates a delete button, and another that creates a toggle button for the book's read status. Added changeRead function to Book.prototype that toggles a book's read property between 'Yes' and 'No' in addition to modifying the DOM to reflect the new value in the 'Read' column.

--- script.js
+++ script.js
@@ -11,45 +11,20 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-// Create a couple of books to place into myLibrary
-
-const dummyBook = new Book("Battle Cry of Freedom", "James McPherson", 862, true);
-const dummyBook2 = new Book("The 48 Laws of Power", "Robert Greene", 430, false);
-
 // We could declare methods in the constructor, but making them on the prototype increases efficiency. All objects that prototypically inherit from Book share this function
 // which results in less memory usage.
 
-Book.prototype.sayTitle = function() {
-    console.log(`The name of this book is '${this.title}'`);
+Book.prototype.changeRead = function() {
+    console.log(`Is '${this.title}' completed? '${this.read}'.`);
+    // If the read property of the book is "Yes," set it to "No" and vice-versa.
+    return this.read = this.read == "Yes" ? "No" : "Yes";
 };
 
-// Make the two objects in myLibrary inherit from Book by using Object.create()
+// Create a couple of books to place into myLibrary
 
-myLibrary.push(dummyBook);
-myLibrary.push(dummyBook2);
-
-// Lists books within a table by appending each entry as a new 'tr' element with 'td' elements that contain the book's information.
-
-function displayLibrary(library){
-    // Iterate over the books of the array.
-    for (let book of library) {
-        // Begin row for new entry.
-        const newRow = document.createElement("tr");
-        // Traverse over each of the properties of a book and retreive their values.
-        for (let property in book) {
-            if (property == "sayTitle") {
-                continue;
-            }
-            // Create cell that will contain a properties' value.
-            const propValue = document.createElement("td");
-            propValue.innerText = book[property];
-            // Add cell to the row as a child of the 'tr' element.
-            newRow.appendChild(propValue);
-        }
-        // Finally, add the entire row that contains all of the book's information to the table.
-        books.appendChild(newRow);
-    }
-}
+const dummyBook = new Book("Battle Cry of Freedom", "James McPherson", 862, "No");
+const dummyBook2 = new Book("The 48 Laws of Power", "Robert Greene", 430, "No");
+const dummyBook3 = new Book("Data Science from Scratch", "Joel Grus", 376, "Yes");
 
 // Select the table element.
 const books = document.getElementById("bookList");
@@ -63,30 +38,98 @@ addBookButton.addEventListener("click", () => {
     dialog.showModal();
 })
 
+// Add a way to close dialog box
+const closeButton = document.getElementById("closeButton");
+closeButton.addEventListener("click", (e) => {
+    dialog.close();
+})
+
 // The submit button needs to retreive all of the values of the input fields of the form and use them to create a new Book object.
 // The values of the input elements can be retreived by selecting the individual input elements by id through the HTMLInputElementAPI (document.getElementById('title') for example)
 
 const diagForm = document.querySelector("form");
 
+// Make the two objects in myLibrary inherit from Book by using Object.create()
+
+myLibrary.push(dummyBook);
+myLibrary.push(dummyBook2);
+myLibrary.push(dummyBook3);
+
 diagForm.addEventListener("submit", (e) => {
+    // Also, need to check the state of the checkbox. If it's checked, the value is 'Yes.'
+    diagForm.elements['read'].value = diagForm.elements['read'].checked ? "Yes" : "No";
     addBookToLibrary(diagForm.querySelectorAll("input"));
     // Erase the input fields upon submission of form.
     diagForm.reset();
 })
+
+// Lists books within a table by appending each entry as a new 'tr' element with 'td' elements that contain the book's information.
+
+function displayLibrary(library){
+    // Iterate over the books of the array.
+    for (let book of library) {
+        // Begin row for new entry.
+        const newRow = document.createElement("tr");
+        // Traverse over each of the properties of a book and retreive their values.
+        for (let property in book) {
+            if (property == "changeRead") {
+                continue;
+            }
+            // Create cell that will contain a properties' value.
+            const propValue = document.createElement("td");
+            propValue.innerText = book[property];
+            // Add cell to the row as a child of the 'tr' element.
+            newRow.appendChild(propValue);
+        }
+        newRow.appendChild(createDeleteButton(book));
+        newRow.appendChild(createToggleButton(book));
+        // Finally, add the entire row that contains all of the book's information to the table.
+        books.appendChild(newRow);
+    }
+}
+
+// Function that creates delete button.
+
+function createDeleteButton(book) {
+    const bookButton = document.createElement("button");
+    bookButton.setAttribute("type", "button");
+    bookButton.setAttribute("class", "deleteButton");
+    // Add click event listener to all deleteButtons. Write code that will remove object from myLibrary. Also need to delete row from the DOM tree and update indices.
+    bookButton.addEventListener("click", () => {
+        myLibrary.splice(`${myLibrary.indexOf(book)}`, 1);
+        bookButton.parentElement.remove();
+    })
+    bookButton.innerText = "Delete";
+    return bookButton;
+}
+
+function createToggleButton(book) {
+    const bookButton = document.createElement("button");
+    bookButton.setAttribute("type", "button");
+    bookButton.setAttribute("class", "toggleButton");
+    // Add click event listener to all deleteButtons. Write code that will remove object from myLibrary. Also need to delete row from the DOM tree and update indices.
+    bookButton.addEventListener("click", () => {
+        bookButton.parentNode.childNodes[3].innerText = book.changeRead();
+    })
+    bookButton.innerText = "Toggle";
+    return bookButton;
+}
 
 // Create function that allows user to add books to library
 
 function addBookToLibrary(formData){
     // Takes information from form to build a new Book, then adds that to myLibrary array.
     // Delaring a variable to store an array of values for each input. Converting from a NodeList to an Array to gain access to map method.
+
     let inputValues = Array.from(formData).map((input) => input.value);
+    // Pass array elements in inputValues one by one using the spread operator (...).
     const newBook = new Book(...inputValues);
     myLibrary.push(newBook);
     // Add new book to table. Start by creating a new row with a 'tr' element.
     const newRow = document.createElement("tr");
     // Traverse over each of the properties of a book and retreive their values.
     for (let property in newBook) {
-        if (property == "sayTitle") {
+        if (property == "changeRead") {
             continue;
         }
         // Create cell that will contain a properties' value.
@@ -95,16 +138,11 @@ function addBookToLibrary(formData){
         // Add cell to the row as a child of the 'tr' element.
         newRow.appendChild(propValue);
     }
+    newRow.appendChild(createDeleteButton(newBook));
     // Finally, add the entire row that contains all of the book's information to the table.
     books.appendChild(newRow);
-    console.log(newBook);
+    // console.log(newBook);
 }
-
-// Add a way to close dialog box
-const closeButton = document.getElementById("closeButton");
-closeButton.addEventListener("click", (e) => {
-    dialog.close();
-})
 
 // Invoke displayLibrary to draw out table of books.
 displayLibrary(myLibrary);
